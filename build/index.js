70a65a72bc8b64c46a58b00b329afee24391019e
Mohamed Amjar <amjar.mohamed.rh@gmail.com>
2024-09-25 05:39:09 +0100

    Resolved conflicts and merged local project with remote

---
+++ build/index.js
@@ -0,0 +1,4 @@
+var a;
+a = { name: "string", age: 33 };
+var nameAm = JSON.parse("550");
+console.log(typeof nameAm);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+type User={
+  name:string,
+  age?:number | string 
+} ;
+
+
+let a : User;
+
+a = {name:"string", age:33};
+
+let nameAm = JSON.parse("550")
+
+console.log(typeof nameAm);
+
